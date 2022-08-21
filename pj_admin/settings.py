a8ff182de791fe1b8b8e27cdbd1918d2669c55ac
unohayato <mamidori1p1p@gmail.com>
2022-08-21 22:10:28 +0900

    modles

--- pj_admin/settings.py
+++ pj_admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')zwr=32ji^$_7__*+y*)ab#nsznqtip6(==(vnfq6dq3=rjtcz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pj_blog',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
---
+++ pj_blog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PjBlogConfig(AppConfig):
+    name = 'pj_blog'
